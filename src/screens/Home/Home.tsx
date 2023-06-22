0062404a932228cb6678eab2de79471fc4b1ab7e
duardoqueiroz <eduardoldq1@gmail.com>
2023-06-22 13:21:42 -0300

    initial commit

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+export default function Home() {
+  return <View style={styles.container}></View>;
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#DC0A2D",
+    width: "100%",
+    height: "100%",
+  },
+});
