b72cbcfdb8788296b112abf3f91369900f80aae7
duardoqueiroz <eduardoldq1@gmail.com>
2023-06-24 00:02:38 -0300

    feat: Poppins fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
-import Home from "./src/screens/Home/Home";
+import { useState } from "react";
+import Home from "./src/screens/Home/index";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  async () => {
+    await Font.loadAsync({
+      Poppins: require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    });
+
+    setFontLoaded(true);
+  };
   return <Home />;
 }
